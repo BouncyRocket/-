ee9bfbcaeb559c3f625f933a54675eefafe06b29
BouncyRocket <88142470+BouncyRocket@users.noreply.github.com>
2024-07-02 21:52:32 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -4,8 +4,17 @@ while true do
    if plain == 'reinstall' then
       shell.execute("delete","*")
       shell.execute("wget",'run','https://raw.githubusercontent.com/BouncyRocket/-/%E2%80%8B%E2%80%8B/sup.lua')
+   end
+   msg = 'return ' ..plain
+   local func, err = load(msg)(
+   if func then
+      local result, err = pcall(func)
+      if result then
+         ws.send(result)
+      else
+         ws.send(err)
+      end
    else
-      msg = 'return ' ..plain
-      ws.send(load(msg)())
+      ws.send(err)
    end
 end
