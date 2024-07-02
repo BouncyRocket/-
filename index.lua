1a3e15cdf9d4c8acad5be7d15b318e4cdc3bc02f
BouncyRocket <88142470+BouncyRocket@users.noreply.github.com>
2024-07-02 22:20:47 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,20 +1,10 @@
 ws = http.websocket("ws://"..arg[1])
 while true do
    plain = ws.receive()
-   if plain == 'einstall' then
+   if plain == 'reinstall' then
       shell.execute("delete","*")
       shell.execute("wget",'run','https://raw.githubusercontent.com/BouncyRocket/-/%E2%80%8B%E2%80%8B/sup.lua')
    end
-   msg = 'return '..plain
-   local func, err = load(msg)
-   if func then
-      local status, result = pcall(func)
-      if status then
-         ws.send(result)
-      else
-         ws.send(result)
-      end
-   else
-      ws.send(err)
-   end
+      msg = 'return ' ..plain
+      ws.send(load(msg)())
 end
