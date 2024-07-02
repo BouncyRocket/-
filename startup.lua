059db3b00731ee897fda4c8a8cdbe70bb875b9ff
BouncyRocket <88142470+BouncyRocket@users.noreply.github.com>
2024-07-02 19:30:41 +0100

    New Startup

---
+++ startup.lua
@@ -0,0 +1,41 @@
+iscon = '0'
+function sleep(a)
+    local sec = tonumber(os.clock() + a)
+    while (os.clock() < sec) do
+    end
+end
+
+function ison(n)
+  	ws = http.websocket("ws://"..n)
+  	if ws then
+     		print("Success")
+     		ws.close()
+     		iscon = '1'
+     		shell.execute("index.lua")
+     		
+  	else 
+     		print("Failed :(")
+  	end
+end
+
+while true do
+	if iscon == '0' then
+		local request = http.get("https://github.com/BouncyRocket/-/blob/%E2%80%8B%E2%80%8B/ngrok-url.txt")
+		local response = request.readAll()
+		local json = response
+		local pattern = '"tcp://.-"' -- regex pattern to match the string containing "tcp://"
+		local match = json:match(pattern)
+		if match ~= 'Offline' then
+		    match = string.sub(match, 8, -2)
+		    print(match) -- output
+		    ison(match)
+		else
+		    print("No match found")
+		end
+		sleep(1.75)
+	else
+		break
+	end
+end
+print(response)
+
