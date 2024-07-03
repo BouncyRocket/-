f6a8a1afd8e30eda161b5902cc795c6c393fa8b7
BouncyRocket <88142470+BouncyRocket@users.noreply.github.com>
2024-07-03 19:38:09 +0100

    Sum

---
+++ disk/di.lua
@@ -0,0 +1,5 @@
+--Disk install for other apps
+shell.execute("wget",'https://raw.githubusercontent.com/BouncyRocket/-/%E2%80%8B%E2%80%8B/startup.lua','../startup.lua')
+shell.execute("wget",'https://raw.githubusercontent.com/BouncyRocket/-/%E2%80%8B%E2%80%8B/index.lua','../index')
+shell.execute("clear")
+os.reboot()
---
+++ disk/startup.lua
@@ -0,0 +1,1 @@
+shell.run('di.lua')
